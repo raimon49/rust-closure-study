a401b5110679f522d6cb6abf77d94b0207986ed1
raimon <raimon49@hotmail.com>
2021-02-27 18:04:02 +0900

    Add a code about using sort function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct City {
+    name: String,
+    population: i64,
+    country: String
+}
+
+// Cityをpopulation順に昇順ソートするためのヘルパー関数
+fn city_popuration_descending(city: &City) -> i64 {
+    -city.population
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cities = vec![City {name: "Tokyo".to_string(), population: 100, country: "Japan".to_string()},
+                      City {name: "NY".to_string(), population: 99, country: "USA".to_string()}];
+
+    cities.sort_by_key(city_popuration_descending);
 }
